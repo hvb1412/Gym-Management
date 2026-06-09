582de6befe6d3cde78923d66e359872de50f8840
vivuong166 <vuongvi221@gmail.com>
2026-06-10 01:19:06 +0700

    eye of phong tap

--- server/src/controllers/room.controller.js
+++ server/src/controllers/room.controller.js
@@ -2,7 +2,7 @@ import { Room } from '../models/index.js';
 
 export const getRooms = async (req, res) => {
     try {
-        const rooms = await Room.findAll({ where: { operatingStatus: 'active' } });
+        const rooms = await Room.findAll();
         res.status(200).json(rooms);
     } catch (error) {
         res.status(500).json({ message: "Lỗi khi lấy danh sách phòng tập", error: error.message });
